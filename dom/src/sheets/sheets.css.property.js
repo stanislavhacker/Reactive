7f7e9b385aa13d24005adb9383d4ac3760fa425d
Stanilav Hacker <info@dbsys.cz>
2015-03-22 10:40:00 +0100

    Css property enum

--- dom/src/sheets/sheets.css.property.js
+++ dom/src/sheets/sheets.css.property.js
@@ -6,6 +6,12 @@
 	"use strict";
 
 	/**
+	 * @private
+	 * @type {Object<string, string>}
+	 */
+	var jsNamesMap = {};
+
+	/**
 	 * Css property
 	 * @param {dom.sheets.CssPropertyType|string} name
 	 * @param {string|dom.data.Contract} value
@@ -73,40 +79,52 @@
 		this.elements.setCssProperty(this);
 	};
 
+
 	/**
-	 * @static
-	 * @param {string} name
+	 * Replace char at
+	 * @param {string} text
+	 * @param {number} index
+	 * @param {string|null} character
 	 * @returns {string}
 	 */
-	dom.sheets.CssProperty.createJsName = function (name) {
-		var delimiter = "-",
-			index = name.indexOf(delimiter);
-
-		/**
-		 * Replace char at
-		 * @param {string} text
-		 * @param {number} index
-		 * @param {string|null} character
-		 * @returns {string}
-		 */
-		function replaceAt(text, index, character) {
-			if (character === null) {
-				return text.substr(0, index) + text.substr(index + 1);
-			}
-			return text.substr(0, index) + character + text.substr(index + character.length);
+	function replaceAt(text, index, character) {
+		if (character === null) {
+			return text.substr(0, index) + text.substr(index + 1);
 		}
+		return text.substr(0, index) + character + text.substr(index + character.length);
+	}
 
+	/**
+	 * @static
+	 * @param {string} cssName
+	 * @returns {string}
+	 */
+	dom.sheets.CssProperty.createJsName = function (cssName) {
+		var index,
+			jsName = cssName,
+			delimiter = "-",
+			nameInCache = jsNamesMap[cssName];
+
+		//return cssName in cache
+		if (nameInCache) {
+			return nameInCache;
+		}
+		//index
+		index = jsName.indexOf(delimiter);
+		//create cssName
 		while (index >= 0) {
 			if (index > 0) {
-				name = replaceAt(name, index, null);
-				name = replaceAt(name, index, name[index].toUpperCase());
+				jsName = replaceAt(jsName, index, null);
+				jsName = replaceAt(jsName, index, jsName[index].toUpperCase());
 			} else {
-				name = replaceAt(name, index, null);
+				jsName = replaceAt(jsName, index, null);
 			}
-			index = name.indexOf(delimiter);
+			index = jsName.indexOf(delimiter);
 		}
-
-		return name;
+		//save cssName into map
+		jsNamesMap[cssName] = jsName;
+		//return new cssName
+		return jsName;
 	};
 
 	/**
@@ -114,11 +132,160 @@
 	 * @enum {string}
 	 */
 	dom.sheets.CssPropertyType = {
-		COLOR: "color",
+		ACCELERATOR: "accelerator",
+		AZIMUTH: "azimuth",
+		BACKGROUND: "background",
+		BACKGROUND_ATTACHMENT: "background-attachment",
+		BACKGROUND_COLOR: "background-color",
+		BACKGROUND_IMAGE: "background-image",
+		BACKGROUND_POSITION: "background-position",
+		BACKGROUND_POSITION_X: "background-position-x",
+		BACKGROUND_POSITION_Y: "background-position-y",
+		BACKGROUND_REPEAT: "background-repeat",
+		BEHAVIOR: "behavior",
 		BORDER: "border",
+		BORDER_BOTTOM: "border-bottom",
+		BORDER_BOTTOM_COLOR: "border-bottom-color",
+		BORDER_BOTTOM_STYLE: "border-bottom-style",
+		BORDER_BOTTOM_WIDTH: "border-bottom-width",
+		BORDER_COLLAPSE: "border-collapse",
 		BORDER_COLOR: "border-color",
+		BORDER_LEFT: "border-left",
+		BORDER_LEFT_COLOR: "border-left-color",
+		BORDER_LEFT_STYLE: "border-left-style",
+		BORDER_LEFT_WIDTH: "border-left-width",
+		BORDER_RIGHT: "border-right",
+		BORDER_RIGHT_COLOR: "border-right-color",
+		BORDER_RIGHT_STYLE: "border-right-style",
+		BORDER_RIGHT_WIDTH: "border-right-width",
+		BORDER_SPACING: "border-spacing",
+		BORDER_STYLE: "border-style",
+		BORDER_TOP: "border-top",
+		BORDER_TOP_COLOR: "border-top-color",
+		BORDER_TOP_STYLE: "border-top-style",
+		BORDER_TOP_WIDTH: "border-top-width",
 		BORDER_WIDTH: "border-width",
-		BORDER_STYLE: "border-style"
-	};
+		BOTTOM: "bottom",
+		CAPTION_SIDE: "caption-side",
+		CLEAR: "clear",
+		CLIP: "clip",
+		COLOR: "color",
+		CONTENT: "content",
+		COUNTER_INCREMENT: "counter-increment",
+		COUNTER_RESET: "counter-reset",
+		CUE: "cue",
+		CUE_AFTER: "cue-after",
+		CUE_BEFORE: "cue-before",
+		CURSOR: "cursor",
+		DIRECTION: "direction",
+		DISPLAY: "display",
+		ELEVATION: "elevation",
+		EMPTY_CELLS: "empty-cells",
+		FILTER: "filter",
+		FLOAT: "float",
+		FONT: "font",
+		FONT_FAMILY: "font-family",
+		FONT_SIZE: "font-size",
+		FONT_SIZE_ADJUST: "font-size-adjust",
+		FONT_STRETCH: "font-stretch",
+		FONT_STYLE: "font-style",
+		FONT_VARIANT: "font-variant",
+		FONT_WEIGHT: "font-weight",
+		HEIGHT: "height",
+		IME_MODE: "ime-mode",
+		INCLUDE_SOURCE: "include-source",
+		LAYER_BACKGROUND_COLOR: "layer-background-color",
+		LAYER_BACKGROUND_IMAGE: "layer-background-image",
+		LAYOUT_FLOW: "layout-flow",
+		LAYOUT_GRID: "layout-grid",
+		LAYOUT_GRID_CHAR: "layout-grid-char",
+		LAYOUT_GRID_CHAR_SPACING: "layout-grid-char-spacing",
+		LAYOUT_GRID_LINE: "layout-grid-line",
+		LAYOUT_GRID_MODE: "layout-grid-mode",
+		LAYOUT_GRID_TYPE: "layout-grid-type",
+		LEFT: "left",
+		LETTER_SPACING: "letter-spacing",
+		LINE_BREAK: "line-break",
+		LINE_HEIGHT: "line-height",
+		LIST_STYLE: "list-style",
+		LIST_STYLE_IMAGE: "list-style-image",
+		LIST_STYLE_POSITION: "list-style-position",
+		LIST_STYLE_TYPE: "list-style-type",
+		MARGIN: "margin",
+		MARGIN_BOTTOM: "margin-bottom",
+		MARGIN_LEFT: "margin-left",
+		MARGIN_RIGHT: "margin-right",
+		MARGIN_TOP: "margin-top",
+		MARKER_OFFSET: "marker-offset",
+		MARKS: "marks",
+		MAX_HEIGHT: "max-height",
+		MAX_WIDTH: "max-width",
+		MIN_HEIGHT: "min-height",
+		MIN_WIDTH: "min-width",
+		ORPHANS: "orphans",
+		OUTLINE: "outline",
+		OUTLINE_COLOR: "outline-color",
+		OUTLINE_STYLE: "outline-style",
+		OUTLINE_WIDTH: "outline-width",
+		OVERFLOW: "overflow",
+		OVERFLOW_X: "overflow-X",
+		OVERFLOW_Y: "overflow-Y",
+		PADDING: "padding",
+		PADDING_BOTTOM: "padding-bottom",
+		PADDING_LEFT: "padding-left",
+		PADDING_RIGHT: "padding-right",
+		PADDING_TOP: "padding-top",
+		PAGE: "page",
+		PAGE_BREAK_AFTER: "page-break-after",
+		PAGE_BREAK_BEFORE: "page-break-before",
+		PAGE_BREAK_INSIDE: "page-break-inside",
+		PAUSE: "pause",
+		PAUSE_AFTER: "pause-after",
+		PAUSE_BEFORE: "pause-before",
+		PITCH: "pitch",
+		PITCH_RANGE: "pitch-range",
+		PLAY_DURING: "play-during",
+		POSITION: "position",
+		QUOTES: "quotes",
+		RICHNESS: "richness",
+		RIGHT: "right",
+		RUBY_ALIGN: "ruby-align",
+		RUBY_OVERHANG: "ruby-overhang",
+		RUBY_POSITION: "ruby-position",
+		SIZE: "size",
+		SPEAK: "speak",
+		SPEAK_HEADER: "speak-header",
+		SPEAK_NUMERAL: "speak-numeral",
+		SPEAK_PUNCTUATION: "speak-punctuation",
+		SPEECH_RATE: "speech-rate",
+		STRESS: "stress",
+		TABLE_LAYOUT: "table-layout",
+		TEXT_ALIGN: "text-align",
+		TEXT_ALIGN_LAST: "text-align-last",
+		TEXT_DECORATION: "text-decoration",
+		TEXT_INDENT: "text-indent",
+		TEXT_JUSTIFY: "text-justify",
+		TEXT_OVERFLOW: "text-overflow",
+		TEXT_SHADOW: "text-shadow",
+		TEXT_TRANSFORM: "text-transform",
+		TEXT_AUTOSPACE: "text-autospace",
+		TEXT_KASHIDA_SPACE: "text-kashida-space",
+		TEXT_UNDERLINE_POSITION: "text-underline-position",
+		TOP: "top",
+		UNICODE_BIDI: "unicode-bidi",
+		VERTICAL_ALIGN: "vertical-align",
+		VISIBILITY: "visibility",
+		VOICE_FAMILY: "voice-family",
+		VOLUME: "volume",
+		WHITE_SPACE: "white-space",
+		WIDOWS: "widows",
+		WIDTH: "width",
+		WORD_BREAK: "word-break",
+		WORD_SPACING: "word-spacing",
+		WORD_WRAP: "word-wrap",
+		WRITING_MODE: "writing-mode",
+		Z_INDEX: "z-index",
+		ZOOM: "zoom"
+};
 
 }());
